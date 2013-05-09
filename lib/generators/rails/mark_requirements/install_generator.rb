3d01047f885fe02a7ebdf285a61aa20d10ef6f8f
Paul Belt <saprb@channing.harvard.edu>
2013-05-09 17:43:48 -0400

    docs

--- lib/generators/rails/mark_requirements/install_generator.rb
+++ lib/generators/rails/mark_requirements/install_generator.rb
@@ -1,4 +1,6 @@
+# :nodoc:
 module Rails
+  # provides a mark_required? helper for rails views
   module MarkRequirements
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
